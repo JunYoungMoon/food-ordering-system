eb2c0e2c4a4b06daded56e12965247128da6d295
JunYoungMoon <ko5642027@naver.com>
2025-11-07 23:10:38 +0900

    주문 애플리케이션 서비스에서 입력 포트 구현하기 (2) 추가로 이벤트 발행하기 1버전으로 분기

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
@@ -3,7 +3,9 @@ package com.food.ordering.system.order.service.domain;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
 import com.food.ordering.system.order.service.domain.entity.Customer;
+import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.entity.Restaurant;
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
 import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
 import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
 import com.food.ordering.system.order.service.domain.ports.output.repository.CustomerRepository;
@@ -43,13 +45,22 @@ public class OrderCreateCommandHandler {
     @Transactional
     public CreateOrderResponse createOrder(CreateOrderCommand createOrderCommand) {
         checkCustomer(createOrderCommand.getCustmerId());
-        Restaurant raRestaurant = checkRestaurant(createOrderCommand);
-        return null;
+        Restaurant restaurant = checkRestaurant(createOrderCommand);
+        Order order = orderDataMapper.createOrderCommandToOrder(createOrderCommand);
+        OrderCreatedEvent orderCreatedEvent = orderDomainService.validateAndInitiateOrder(order, restaurant);
+        Order orderResult = saveOrder(order);
+        log.info("Order is created with id: {}", orderResult.getId().getValue());
+        return orderDataMapper.orderToCreateOrderResponse(orderResult);
     }
 
     private Restaurant checkRestaurant(CreateOrderCommand createOrderCommand) {
-
-
+        Restaurant restaurant = orderDataMapper.createOrderCommmandRestaurant(createOrderCommand);
+        Optional<Restaurant> optionalRestaurant = restaurantRepository.findRestaurantInformation(restaurant);
+        if (optionalRestaurant.isEmpty()) {
+            log.warn("Could not find restaurant with restaurant id: {}", createOrderCommand.getRestaurantId());
+            throw new OrderDomainException("Could not find restaurant with restaurant id: " + createOrderCommand.getRestaurantId());
+        }
+        return optionalRestaurant.get();
     }
 
     private void checkCustomer(UUID custmerId) {
@@ -59,5 +70,15 @@ public class OrderCreateCommandHandler {
             log.warn("Could not find customer with customer id: {}", custmerId);
             throw new OrderDomainException(String.format("Could not find customer with customer id: %s", custmerId));
         }
+    }
+
+    private Order saveOrder(Order order){
+        Order orderResult = orderRepository.save(order);
+        if(orderResult == null){
+            log.error("Could not save order!");
+            throw new OrderDomainException("Could not save order!");
+        }
+        log.info("Order is saved with id : {}", orderResult.getId().getValue());
+        return orderResult;
     }
 }
