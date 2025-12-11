7df513e3b76572043a9a636d50ac74aef26e7fe9
JunYoungMoon <ko5642027@naver.com>
2025-12-12 00:19:00 +0900

    컨테이너 모듈 구현하기 1

--- payment-service/payment-container/src/main/java/com/food/ordering/system/payment/service/domain/PaymentServiceApplication.java
+++ payment-service/payment-container/src/main/java/com/food/ordering/system/payment/service/domain/PaymentServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.food.ordering.system.")
+@EnableJpaRepositories(basePackages = "com.food.ordering.system.payment.service.dataaccess")
 @EntityScan(basePackages = "com.food.ordering.system.payment.service.dataaccess")
 @SpringBootApplication(scanBasePackages = "com.food.ordering.system")
 public class PaymentServiceApplication {
