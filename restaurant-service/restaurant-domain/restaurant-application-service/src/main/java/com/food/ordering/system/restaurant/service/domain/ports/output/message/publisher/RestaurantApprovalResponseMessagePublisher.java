e58c40b5396878e44ba043e76f04b8853ae72237
JunYoungMoon <ko5642027@naver.com>
2025-12-18 00:09:12 +0900

    애플리케이션 서비스 도메인 모듈: 매퍼, DTO 및 포트 추가하기

--- restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/ports/output/message/publisher/RestaurantApprovalResponseMessagePublisher.java
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/ports/output/message/publisher/RestaurantApprovalResponseMessagePublisher.java
@@ -1,12 +1,8 @@
 package com.food.ordering.system.restaurant.service.domain.ports.output.message.publisher;
 
-import com.food.ordering.system.outbox.OutboxStatus;
-import com.food.ordering.system.restaurant.service.domain.outbox.model.OrderOutboxMessage;
-
-import java.util.function.BiConsumer;
+import com.food.ordering.system.restaurant.service.domain.dto.RestaurantApprovalRequest;
 
 public interface RestaurantApprovalResponseMessagePublisher {
 
-    void publish(OrderOutboxMessage orderOutboxMessage,
-                 BiConsumer<OrderOutboxMessage, OutboxStatus> outboxCallback);
+    void approveOrder(RestaurantApprovalRequest restaurantApprovalRequest);
 }
