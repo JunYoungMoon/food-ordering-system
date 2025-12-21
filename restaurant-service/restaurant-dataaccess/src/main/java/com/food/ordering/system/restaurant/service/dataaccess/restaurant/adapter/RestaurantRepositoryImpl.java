cdbb6f96e73aa0c143f64c7f7cbf9169a7076b7e
JunYoungMoon <ko5642027@naver.com>
2025-12-21 23:01:57 +0900

    데이터 액세스 모듈 구현하기 1

--- restaurant-service/restaurant-dataaccess/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restaurant/adapter/RestaurantRepositoryImpl.java
+++ restaurant-service/restaurant-dataaccess/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restaurant/adapter/RestaurantRepositoryImpl.java
@@ -1,7 +1,5 @@
 package com.food.ordering.system.restaurant.service.dataaccess.restaurant.adapter;
 
-import com.food.ordering.system.dataaccess.restaurant.entity.RestaurantEntity;
-import com.food.ordering.system.dataaccess.restaurant.repository.RestaurantJpaRepository;
 import com.food.ordering.system.restaurant.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
 import com.food.ordering.system.restaurant.service.domain.entity.Restaurant;
 import com.food.ordering.system.restaurant.service.domain.ports.output.repository.RestaurantRepository;
