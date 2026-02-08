dd0f301c67f8931e718aea7f655319c762d8849e
JunYoungMoon <ko5642027@naver.com>
2026-02-08 23:48:50 +0900

    아웃박스 패턴 변경에 대한 주문 애플리케이션 서비스 테스트 업데이트하기 테스트 코드 수정

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainService.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainService.java
@@ -10,7 +10,7 @@ import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 import java.util.List;
 
 public interface OrderDomainService {
-    OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant, DomainEventPublisher<OrderCreatedEvent> orderCreatedEventDomainEventPublisher);
+    OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant);
     OrderPaidEvent payOrder(Order order);
     void approveOrder(Order order);
     OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages);
