82dfea981239048d19fd7a08c4960160f69b49d7
JunYoungMoon <ko5642027@naver.com>
2026-02-15 23:52:35 +0900

    아웃박스 패턴에 대한 주문 메시징 모듈 리팩터링 - 2부

--- order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/publisher/kafka/OrderPaymentEventKafkaPublisher.java
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/publisher/kafka/OrderPaymentEventKafkaPublisher.java
@@ -1,12 +1,10 @@
 package com.food.ordering.system.order.service.messaging.publisher.kafka;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.kafka.order.avro.model.RestaurantApprovalRequestAvroModel;
 import com.food.ordering.system.kafka.producer.KafkaMessageHelper;
 import com.food.ordering.system.kafka.producer.service.KafkaProducer;
 import com.food.ordering.system.order.service.domain.config.OrderServiceConfigData;
-import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
 import com.food.ordering.system.order.service.domain.outbox.model.payment.OrderPaymentEventPayload;
 import com.food.ordering.system.order.service.domain.outbox.model.payment.OrderPaymentOutboxMessage;
 import com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.PaymentRequestMessagePublisher;
@@ -27,26 +25,23 @@ public class OrderPaymentEventKafkaPublisher implements PaymentRequestMessagePub
     private final KafkaProducer<String, PaymentRequestAvroModel> kafkaProducer;
     private final OrderServiceConfigData orderServiceConfigData;
     private final KafkaMessageHelper kafkaMessageHelper;
-    private final ObjectMapper objectMapper;
 
     public OrderPaymentEventKafkaPublisher(OrderMessagingDataMapper orderMessagingDataMapper,
                                            KafkaProducer<String, PaymentRequestAvroModel> kafkaProducer,
                                            OrderServiceConfigData orderServiceConfigData,
-                                           KafkaMessageHelper kafkaMessageHelper,
-                                           ObjectMapper objectMapper) {
+                                           KafkaMessageHelper kafkaMessageHelper) {
         this.orderMessagingDataMapper = orderMessagingDataMapper;
         this.kafkaProducer = kafkaProducer;
         this.orderServiceConfigData = orderServiceConfigData;
         this.kafkaMessageHelper = kafkaMessageHelper;
-        this.objectMapper = objectMapper;
     }
 
     @Override
     public void publish(OrderPaymentOutboxMessage orderPaymentOutboxMessage,
                         BiConsumer<OrderPaymentOutboxMessage, OutboxStatus> outboxCallback) {
-
         OrderPaymentEventPayload orderPaymentEventPayload =
-                getOrderPaymentEventPayload(orderPaymentOutboxMessage.getPayload());
+                kafkaMessageHelper.getOrderEventPayload(orderPaymentOutboxMessage.getPayload(),
+                        OrderPaymentEventPayload.class);
 
         String sagaId = orderPaymentOutboxMessage.getSagaId().toString();
 
@@ -61,52 +56,19 @@ public class OrderPaymentEventKafkaPublisher implements PaymentRequestMessagePub
             PaymentRequestAvroModel paymentRequestAvroModel = orderMessagingDataMapper
                     .orderPaymentEventToPaymentRequestAvroModel(sagaId, orderPaymentEventPayload);
 
-            kafkaProducer.send(
-                    orderServiceConfigData.getPaymentRequestTopicName(),
+            kafkaProducer.send(orderServiceConfigData.getPaymentRequestTopicName(),
                     sagaId,
                     paymentRequestAvroModel,
-                    future
-            );
-
-            future.whenComplete((result, throwable) -> {
-                if (throwable != null) {
-                    // 실패: 로깅 + Outbox 업데이트
-                    kafkaMessageHelper.handleFailure(
-                            orderPaymentEventPayload.getOrderId(),
-                            paymentRequestAvroModel,
-                            throwable,
-                            orderPaymentOutboxMessage,
-                            outboxCallback
-                    );
-                } else {
-                    // 성공: 로깅 + Outbox 업데이트
-                    kafkaMessageHelper.handleSuccess(
-                            orderPaymentEventPayload.getOrderId(),
-                            paymentRequestAvroModel,
-                            result,
-                            orderPaymentOutboxMessage,
-                            outboxCallback
-                    );
-                }
-            });
+                    future);
 
             log.info("OrderPaymentEventPayload sent to Kafka for order id: {} and saga id: {}",
                     orderPaymentEventPayload.getOrderId(), sagaId);
-
         } catch (Exception e) {
-            log.error("Error while sending OrderPaymentEventPayload" +
-                            " to kafka with order id: {} and saga id: {}, error: {}",
-                    orderPaymentEventPayload.getOrderId(), sagaId, e.getMessage());
-            outboxCallback.accept(orderPaymentOutboxMessage, OutboxStatus.FAILED);
+           log.error("Error while sending OrderPaymentEventPayload" +
+                           " to kafka with order id: {} and saga id: {}, error: {}",
+                   orderPaymentEventPayload.getOrderId(), sagaId, e.getMessage());
         }
-    }
 
-    private OrderPaymentEventPayload getOrderPaymentEventPayload(String payload) {
-        try {
-            return objectMapper.readValue(payload, OrderPaymentEventPayload.class);
-        } catch (JsonProcessingException e) {
-            log.error("Could not read OrderPaymentEventPayload object!", e);
-            throw new OrderDomainException("Could not read OrderPaymentEventPayload object!", e);
-        }
+
     }
 }
